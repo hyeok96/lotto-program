f8385c0c881fd039747ecab4aed6914cc1fe0d65
sang hyeok Lee <saing0813@naver.com>
2023-10-10 18:48:43 +0900

    output Lottos

--- src/main/java/com/timber/lotto/LottoProgram.java
+++ src/main/java/com/timber/lotto/LottoProgram.java
@@ -1,12 +1,16 @@
 package com.timber.lotto;
 
+import com.timber.lotto.domain.lotto.AutoLottoGenerator;
 import com.timber.lotto.domain.lotto.Lotto;
 import com.timber.lotto.domain.Money;
+import com.timber.lotto.domain.lotto.LottoNumber;
 import com.timber.lotto.domain.lotto.ManualLottoGenerator;
 import com.timber.lotto.view.InputView;
 import com.timber.lotto.view.OutputView;
 
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 public class LottoProgram {
@@ -19,7 +23,12 @@ public class LottoProgram {
         int manualLottoNum = inputView.inputManualLottoNum(money.getValue(), Lotto.LOTTO_PRICE);
         money.spend(manualLottoNum * Lotto.LOTTO_PRICE);
 
-        List<Lotto> manualLottos = getManualLottos(manualLottoNum);
+        List<Lotto> allLottos = new ArrayList<>();
+        allLottos.addAll(getManualLottos(manualLottoNum));
+        allLottos.addAll(getAutomaticLottos(money.getValue()));
+
+        System.out.println(allLottos);
+
     }
 
     private List<Lotto> getManualLottos(int manualLottoNum) throws IOException {
@@ -28,6 +37,15 @@ public class LottoProgram {
                 .map(ManualLottoGenerator::new)
                 .map(Lotto::new)
                 .toList();
+    }
+
+    private List<Lotto> getAutomaticLottos(BigDecimal money) {
+        List<Lotto> automaticLottos = new ArrayList<>();
+        int numberOfAutomaticLotto = new BigDecimal(String.valueOf(money)).intValue() / Lotto.LOTTO_PRICE;
+        for (int i = 0; i < numberOfAutomaticLotto; i++) {
+            automaticLottos.add(new Lotto(new AutoLottoGenerator()));
+        }
+        return automaticLottos;
     }
 }
 
